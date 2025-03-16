b70a2991e7e500a1540220e75e50957eacf93e2b
foython <foython@gmail.com>
2025-03-17 00:44:03 +0600

    link update for cart

--- static/updateCart.js
+++ static/updateCart.js
@@ -14,20 +14,20 @@ function getCookie(name) {
     return cookieValue;
 }
 const csrftoken = getCookie('csrftoken');
-  
-  
-document.addEventListener('DOMContentLoaded', ()=>{ 
-    document.querySelectorAll('.qty-minus').forEach(function(item){
-        item.onclick = ()=>{ 
-            var counter = item.dataset.counter;  
-            var effect = document.getElementById(`qty${counter}`); 
-            var qty = effect.value;      
-            if( !isNaN( qty ) && qty > 0 ){
-            effect.value--; 
-            } 
-            var productId = item.dataset.product;            
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.qty-minus').forEach(function (item) {
+        item.onclick = () => {
+            var counter = item.dataset.counter;
+            var effect = document.getElementById(`qty${counter}`);
+            var qty = effect.value;
+            if (!isNaN(qty) && qty > 0) {
+                effect.value--;
+            }
+            var productId = item.dataset.product;
             var action = 'minus';
-            var data = {productId: productId, action: action};
+            var data = { productId: productId, action: action };
             updateReq(data)
         }
     })
@@ -35,21 +35,21 @@ document.addEventListener('DOMContentLoaded', ()=>{
 })
 
 
-document.addEventListener('DOMContentLoaded', ()=>{ 
-    document.querySelectorAll('.qty-plus').forEach(function(item){
-        item.onclick = ()=>{ 
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.qty-plus').forEach(function (item) {
+        item.onclick = () => {
             var counter = item.dataset.counter;
-            var effect = document.getElementById(`qty${counter}`); 
-            var qty = effect.value;      
-            if( !isNaN( qty )){
-            effect.value++;
-            }                
-            var productId = item.dataset.product;            
+            var effect = document.getElementById(`qty${counter}`);
+            var qty = effect.value;
+            if (!isNaN(qty)) {
+                effect.value++;
+            }
+            var productId = item.dataset.product;
             var action = 'add';
             console.log(productId)
-            var data = {productId: productId, action: action};
+            var data = { productId: productId, action: action };
             updateReq(data)
-             
+
         }
     })
 
@@ -59,51 +59,51 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 
 
-function updateReq(data){
-    var url = 'http://127.0.0.1:8000/update/cart/';
+function updateReq(data) {
+    var url = 'https://ecommerce-j3yd.onrender.com//update/cart/';
 
     fetch(url, {
         method: 'POST',
-        headers:{
-        'Content-Type': 'application/json',
-        'X-CSRFToken': csrftoken, //Necessary to work with request.is_ajax()          
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken, //Necessary to work with request.is_ajax()          
         },
 
-        body: JSON.stringify(data) 
+        body: JSON.stringify(data)
     })
-    .then((response)=>{       
-        return response.json()
-    })
-    .then(data => {        
-        console.log('Success:', data);        
-        console.log(Object.values(data));
-        console.log(data);
-        document.querySelectorAll('.cart-item').forEach(function(item){
-           qty = item.querySelector('.qty-text').value;
-           if(qty == 0){
-            item.style.setProperty("display", "none", "important");
-           }
+        .then((response) => {
+            return response.json()
         })
-                
-    });
+        .then(data => {
+            console.log('Success:', data);
+            console.log(Object.values(data));
+            console.log(data);
+            document.querySelectorAll('.cart-item').forEach(function (item) {
+                qty = item.querySelector('.qty-text').value;
+                if (qty == 0) {
+                    item.style.setProperty("display", "none", "important");
+                }
+            })
+
+        });
 }
 
 
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#customRadio3').checked = true;
-    document.querySelectorAll('.custom-control').forEach(function(item){
-        item.onclick = ()=>{
-            var price = item.dataset.price;            
-            if(price === '0.0'){
+    document.querySelectorAll('.custom-control').forEach(function (item) {
+        item.onclick = () => {
+            var price = item.dataset.price;
+            if (price === '0.0') {
                 document.querySelector('#delivery-price').innerHTML = 'Free';
             }
-            else{
-                document.querySelector('#delivery-price').innerHTML = '$'+price;
-            }             
-            var cartTotal = document.querySelector('.cart-total-chart').dataset.total;           
+            else {
+                document.querySelector('#delivery-price').innerHTML = '$' + price;
+            }
+            var cartTotal = document.querySelector('.cart-total-chart').dataset.total;
             cartTotal = Number(cartTotal);
             cartTotal += Number(price)
-            document.querySelector('#total-cart').innerHTML = '$'+cartTotal.toFixed(2)
+            document.querySelector('#total-cart').innerHTML = '$' + cartTotal.toFixed(2)
         }
     })
 });
